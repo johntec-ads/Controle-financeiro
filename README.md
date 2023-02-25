8e9f9bb2eb3fe976d5ae8bb717573a12a7a8562d
johntec-ads <johntec.ads@gmail.com>
2023-02-25 15:48:55 -0300

    Atualização

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Controle-financeiro
 Aplicação para controle de despesas
+
+const getExpense = transactionsAmounts => Math.abs(transactionsAmounts
+	.filter((value) => value < 0)
+	.reduce((accumulator, transaction) => accumulator + transaction, 0))
+	.toFixed(2);
+	
+const getIncome = transactionsAmounts => transactionsAmounts
+	.filter(value => value > 0)
+	.reduce((accumulator, transaction) => accumulator + transaction, 0)
+	.toFixed(2);
+
+const getTotal = transactionsAmounts => transactionsAmounts
+	.reduce((accumulator, transaction) => accumulator + transaction, 0)
+	.toFixed(2);
+
+const convertNumberToReal = number => {
+	console.log(number.toFixed(0).split('.')[0].split(/(?=(?:...)*$)/).join('.'))
+	let newNumber = `${number.toFixed(0).split('.')[0].split(/(?=(?:...)*$)/)
+	.join('.')},${number.toFixed(2).split('.')[1]}`
+	console.log(newNumber)
+	return newNumber
+}
+
+
+
+const updateBalanceValues = () =>{
+	const transactionsAmounts = transactions.map(({ amount }) => amount);
+	
+	const total = convertNumberToReal( Number( getTotal(transactionsAmounts) ) );
+	
+	const income = convertNumberToReal( Number( getIncome(transactionsAmounts) ) );
+	
+	const expense = convertNumberToReal( Number( getExpense(transactionsAmounts) ) );
+
+	incomeDisplay.innerText = `R$ ${income}`;
+	expenseDisplay.innerText = `- R$ ${expense}`;
+	balanceDisplay.innerText = `R$ ${total}`;
+}
+
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const transactionUl = document.querySelector('#transactions');
+const transactionUl = document.querySelector('#transactions');//Variável recebe à id da (ul do html)
 const incomeDisplay = document.querySelector('#money-plus');
 const expenseDisplay = document.querySelector('#money-minus');
 const balanceDisplay = document.querySelector('#balance');
@@ -6,6 +6,7 @@ const form = document.querySelector('#form');
 const inputTransactionName = document.querySelector('#text')
 const inputTransactionAmount = document.querySelector('#amount')
 
+//API salva os dados no local storage.
 const localStorageTransactions = JSON.parse(localStorage
   .getItem('transactions'))
 let transactions = localStorage
@@ -21,49 +22,82 @@ const removeTransaction = ID => {
 
 //Adicionar as transações do DOM.
 const addTransactionIntoDOM = ({ amount, name, id }) => {
-  const operator = amount < 0 ? '-' : '+'
-  const CSSClass = amount < 0 ? 'minus' : 'plus'
+  const operator = amount < 0 ? '-' : '+';//Menor que 0, recebe a string (-),senão (+).   
+  const CSSClass = amount < 0 ? 'minus' : 'plus';//String para uma id class:(minus),ou (plus).
   const amountWithoutOperator = Math.abs(amount);
-  const li = document.createElement('li');
+  const li = document.createElement('li');//Criação da linha HTML.
+  li.classList.add(CSSClass)//Criando o elemento (li) e adicionando a (CSSClass -> nome da classe).
 
-  li.classList.add(CSSClass)
+  /* Setando a marcação interna dentro da (li) com o li.innerHTML */
   li.innerHTML = `
   ${name} <span>${operator} R$ ${amountWithoutOperator}</span>
    <button class="delete-btn" onClick="removeTransaction(${id})">x</button>
   `
   transactionUl.append(li);
+  /* Metodo append(Argumento)-insere o elemento como último filho.
+    Metodo prepend(Argumento)-insere o elemento com primeiro filho */
 
 }
 
-const getExpenses = transactionAmounts => Math.abs(transactionAmounts
-  .filter(value => value < 0)
-  .reduce((accumulator, value) => accumulator + value, 0))
+const getExpenses = transactionAmounts => Math.abs
+  (transactionAmounts
+    .filter(value => value < 0)
+    .reduce((accumulator, value) => accumulator
+      + value, 0))
   .toFixed(2);
 
-const getIncome = transactionAmounts => transactionAmounts
-  .filter(value => value > 0)
-  .reduce((accumulator, value) => accumulator + value, 0)
-  .toFixed(2);
+const getIncome = transactionAmounts =>
+  transactionAmounts
+    .filter(value => value > 0)
+    .reduce((accumulator, value) => accumulator
+      + value, 0)
+    .toFixed(2);
 
-const getTotal = transactionAmounts => transactionAmounts
-  .reduce((accumulator, transaction) => accumulator + transaction, 0)
-  .toFixed(2);
+const getTotal = transactionAmounts =>
+  transactionAmounts
+    .reduce((accumulator, transaction) => accumulator
+      + transaction, 0)
+    .toFixed(2);
+
+
+/* Mudança no código, sugerido por seguidor do youtube */
+const convertNumberToReal = number => {
+  console.log(number.toFixed(0).split('.')[0].split(/(?=(?:...)*$)/).join('.'))
+  let newNumber = `${number.toFixed(0).split('.')[0].split(/(?=(?:...)*$)/)
+    .join('.')},${number.toFixed(2).split('.')[1]}`
+  console.log(newNumber)
+  return newNumber
+}
+
 
 
 const updateBalanceValues = () => {
-  const transactionAmounts = transactions.map(({amount}) => amount);
-  
-  const total = getTotal(transactionAmounts);
-  const income = getIncome(transactionAmounts);
-  const expense = getExpenses(transactionAmounts);
+  const transactionAmounts = transactions.map(({ amount }) =>
+    amount);
 
-  balanceDisplay.textContent = `R$ ${total}`;
-  incomeDisplay.textContent = `R$ ${income}`;
-  expenseDisplay.textContent = `R$ ${expense}`;
+  /* Mudança no código */
+  //Original
+  /* const total = getTotal(transactionAmounts); */
+
+  /* Alterado */
+
+
+  const total = convertNumberToReal(Number(getTotal
+    (transactionAmounts)));
+
+  const income = convertNumberToReal(Number(getIncome(transactionAmounts)));
+
+  const expense = convertNumberToReal(Number(getExpenses(transactionAmounts)));
+
+
+
+  incomeDisplay.innerText = `R$ ${income}`;
+	expenseDisplay.innerText = `- R$ ${expense}`;
+	balanceDisplay.innerText = `R$ ${total}`;
 
 
 }
-
+/* Função que adiciona as transações no DOM , sempre que a pag for carregada */
 const init = () => {
   transactionUl.innerHTML = '';
   transactions.forEach(addTransactionIntoDOM);
@@ -76,7 +110,7 @@ const updateLocalStorage = () => {
   localStorage.setItem('transactions', JSON.stringify(transactions))
 }
 
-const generateID = () => Math.round(Math.random() * 1000)
+const generateID = () => (Math.random() * 1000)
 
 const addToTransactionsArray = (transactionName, transactionAmount) => {
   transactions.push({
@@ -105,6 +139,7 @@ const handFormSubmit = event => {
 
   addToTransactionsArray(transactionName, transactionAmount);
   init();
+
   updateLocalStorage();
 
   cleanInputs();
